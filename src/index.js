a20e7e40d234df7e85f102a720fb6f716d5906d3
GerboPawa <63540672+GerboPawa@users.noreply.github.com>
2025-03-22 12:44:06 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,31 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './styles/global.css';
 
+// Importa i18next e le dipendenze
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+
+// Inizializza i18next
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    fallbackLng: 'en',
+    detection: {
+      order: ['htmlTag', 'cookie', 'localStorage', 'sessionStorage', 'navigator', 'querystring', 'path', 'subdomain'],
+      caches: ['localStorage', 'cookie'],
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/translation.json',
+    },
+    react: {
+      useSuspense: false
+    }
+  });
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 
