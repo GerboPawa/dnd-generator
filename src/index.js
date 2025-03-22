0f3a422b6bb0c9812db22d661ee8d034f7f50362
GerboPawa <63540672+GerboPawa@users.noreply.github.com>
2025-03-22 14:10:23 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,29 +5,7 @@ import App from './App';
 import './styles/global.css';
 
 // Importa i18next e le dipendenze
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from './i18next-browser-languagedetector/index.js';
-import HttpApi from './i18next-http-backend/index.js';
-
-// Inizializza i18next
-i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .use(LanguageDetector)
-  .use(HttpApi)
-  .init({
-    fallbackLng: 'en',
-    detection: {
-      order: ['htmlTag', 'cookie', 'localStorage', 'sessionStorage', 'navigator', 'querystring', 'path', 'subdomain'],
-      caches: ['localStorage', 'cookie'],
-    },
-    backend: {
-      loadPath: '/locales/{{lng}}/translation.json',
-    },
-    react: {
-      useSuspense: false
-    }
-  });
+import i18n from './i18n/i18n'; // Importa l'istanza di i18next dal file di configurazione
 
 const container = document.getElementById('root');
 const root = createRoot(container);
